b8a464304a276f1012e84a5cba2df6d80d976dbf
daanjiri <da.jimenez166@gmail.com>
2025-02-08 10:08:22 -0500

    first commit

---
+++ app/verification/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Verification() {
+  return (
+    <View>
+      <Text>Verification</Text>
+    </View>
+  ); // <-- Ensure proper closing
+}
